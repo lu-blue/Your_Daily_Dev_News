45d552d42bd9fcf47a7dd3b85e406efbd3ac0539
lu-blue <lubov.novozhilova@gmail.com>
2021-04-12 15:02:29 +0200

    Added @RequestMapping("/articles"), CRUD Controller finished

--- src/main/java/se/sdaproject/NewsController.java
+++ src/main/java/se/sdaproject/NewsController.java
@@ -5,8 +5,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.module.ResolutionException;
 import java.util.List;
 
+@RequestMapping("/articles")
 @RestController
 public class NewsController {
 
@@ -18,14 +20,14 @@ public class NewsController {
     }
 
     //RETURNS ALL ARTICLES
-    @GetMapping("/articles")
+    @GetMapping
     public List<News> listAllArticles(){
         List<News> articles = newsRepository.findAll();
         return articles;
     }
 
     //RETURNS ARTICLE BY ID
-    @GetMapping("/articles/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<News> getArticle(@PathVariable Long id){
         News article = newsRepository.
                 findById(id).orElseThrow(ResourceNotFoundException::new);
@@ -33,19 +35,26 @@ public class NewsController {
     }
 
     //UPDATES ARTICLE
-    @PutMapping("/articles/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<News> updateArticle(@PathVariable Long id, @RequestBody News updatedArticle) {
         newsRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
         updatedArticle.setId(id);
         News article = newsRepository.save(updatedArticle);
         return ResponseEntity.ok(article);
+    }
 
+    //DELETES ARTICLE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<News> deleteArticle(@PathVariable Long id) {
+        News article = newsRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        newsRepository.delete(article);
+        return ResponseEntity.ok(article);
     }
 
     //CREATES A NEW ARTICLE
     // "save" will create an object you input and persist it. It´s a post request
     // (saves, but if in application.properties in the last line is "create" instead of "validate", the table will be dropped if we stop Springboot)
-    @PostMapping("/articles")
+    @PostMapping
     public ResponseEntity<News> createArticle(@RequestBody News article) {
         newsRepository.save(article);
         return ResponseEntity.status(HttpStatus.CREATED).body(article);
