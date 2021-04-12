05b213845eeb23baacb749d544b532e354724525
lu-blue <lubov.novozhilova@gmail.com>
2021-04-12 14:40:04 +0200

    Returns article by ID and Updates article

--- src/main/java/se/sdaproject/News.java
+++ src/main/java/se/sdaproject/News.java
@@ -27,6 +27,14 @@ public class News {
         this.authorName = body;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getTitle() {
         return title;
     }
--- src/main/java/se/sdaproject/NewsController.java
+++ src/main/java/se/sdaproject/NewsController.java
@@ -1,9 +1,9 @@
 package se.sdaproject;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,20 +17,38 @@ public class NewsController {
         this.newsRepository = newsRepository;
     }
 
-    //will show an empty array for people
-    @RequestMapping("/articles")
+    //RETURNS ALL ARTICLES
+    @GetMapping("/articles")
     public List<News> listAllArticles(){
         List<News> articles = newsRepository.findAll();
         return articles;
     }
 
-    // "save" will create an object you input and persist it
+    //RETURNS ARTICLE BY ID
+    @GetMapping("/articles/{id}")
+    public ResponseEntity<News> getArticle(@PathVariable Long id){
+        News article = newsRepository.
+                findById(id).orElseThrow(ResourceNotFoundException::new);
+        return ResponseEntity.ok(article);
+    }
+
+    //UPDATES ARTICLE
+    @PutMapping("/articles/{id}")
+    public ResponseEntity<News> updateArticle(@PathVariable Long id, @RequestBody News updatedArticle) {
+        newsRepository.findById(id).orElseThrow(ResourceNotFoundException::new);
+        updatedArticle.setId(id);
+        News article = newsRepository.save(updatedArticle);
+        return ResponseEntity.ok(article);
+
+    }
+
+    //CREATES A NEW ARTICLE
+    // "save" will create an object you input and persist it. It´s a post request
     // (saves, but if in application.properties in the last line is "create" instead of "validate", the table will be dropped if we stop Springboot)
-    @RequestMapping("/articles/create/{title}/{body}/{authorName}")
-    public News createArticle(@PathVariable("title") String title, @PathVariable("body") String body, @PathVariable("authorName") String authorName) {
-        News article = new News(title, body, authorName);
+    @PostMapping("/articles")
+    public ResponseEntity<News> createArticle(@RequestBody News article) {
         newsRepository.save(article);
-        return article;
+        return ResponseEntity.status(HttpStatus.CREATED).body(article);
     }
 
 
